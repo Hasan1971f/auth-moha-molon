cdcf9f172ddd95745ae5bfb431d07a30e736279c
Hasan1971f <mdhasanurrahmanhasan101@gmail.com>
2024-12-25 20:58:37 +0600

    added a Login using context and explain on auth state changed

--- src/Providers/AuthProvider.jsx
+++ src/Providers/AuthProvider.jsx
@@ -1,18 +1,36 @@
-import { createUserWithEmailAndPassword } from 'firebase/auth';
-import React, { createContext } from 'react';
+import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword } from 'firebase/auth';
+import React, { createContext, useState } from 'react';
 import { auth } from '../firebase.init';
  export const AuthContext = createContext(null)
 const AuthProvider = ({children}) => {
-
+    cnst [user, setUser] = useState(null)
     const name = 'Potato alu mia'
+    
 
     const createUser = (email, password)=>{
         return createUserWithEmailAndPassword(auth, email, password)
     }
 
+    const signInUser = (email, password)=>{
+        return signInWithEmailAndPassword(auth, email, password)
+    }
+
+    // onAuthStateChanged(auth, currentUser=>{
+    //     if (currentUser) {
+    //         console.log('cuurrently loged user', currentUser)
+    //         setUser( currentUser)
+    //     }
+    //     else{
+    //         console.log('No user logged in')
+    //         setUser(null)
+    //     }
+    // })
+
     const authInfo = {
        name,
-       createUser
+       user,
+       createUser,
+       signInUser
     }
 
     return (
